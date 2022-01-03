9b70ff46e2d58c551e98765e7597e0fa218760e1
PavelSerebryany <pavel97107@gmail.com>
2022-01-03 17:06:11 +0300

    install find-my-way routing library

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,33 @@
 const http = require('http');
-
+const router = require('find-my-way')()
 const hostname = '127.0.0.1';
 const port = 3000;
 
-const server = http.createServer((req, res) => {
+
+router.on('GET', '/', function (req, res, params) {
+    res.statusCode = 200;
+    res.end('home page')
+})
+
+
+router.on('GET', '/about', function (req, res, params) {
 
     res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
+    res.end('about page')
+})
 
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    router.lookup(req, res);
     res.end("Hello world");
 });
 
 
-
-server.listen(port, hostname, () => {
+server.listen(port, hostname, (err) => {
+    if(err) {
+        throw new Error(err);
+    }
     console.log(`Server running at http://${hostname}:${port}/`);
 });
 
